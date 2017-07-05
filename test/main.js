05ed2db9ae9a42c06e9515f41f21d3cf0068700e
Igor Purtov <ipurtov@toidev.com>
2017-07-05 14:58:23 +0300

    split on components

--- test/main.js
+++ test/main.js
@@ -7,11 +7,35 @@ var data = {
   newItem: ''
 }
 
-new Vue({
-  el: '#app',
-  data: data,
+var ItemsComponent = Vue.extend({
+  data: function(){
+    return data;
+  },
+  template: `
+    <ul>
+      <li v-for="item in items" v-bind:class="{'removed': item.checked}">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" v-model="item.checked"> {{ item.text }}
+          </label>
+        </div>
+      </li>
+    </ul>`
+});
+
+var ChangeTitleComponent = Vue.extend({
+  data: function(){
+    return data;
+  },
+  template: '<input v-model="title">'
+});
+
+var AddItemComponent = Vue.extend({
+  data: function(){
+    return data;
+  },
   methods: {
-    addItem: function(){
+    addItem: function(e){
       var text;
       text = this.newItem.trim();
       if (text){
@@ -20,7 +44,24 @@ new Vue({
           checked: false
         });
         this.newItem = '';
+        $(e.currentTarget).focus();
       }
     }
-  }
+  },
+  template: `
+    <div class="input-group">
+      <input v-model="newItem" @keyup.enter="addItem" placeholder="add shoping list item" type="text" class="form-control">
+      <span class="input-group-btn">
+        <button @click="addItem" type="button" class="btn btn-default">Add!</button>
+      </span>
+    </div>`
+})
+
+Vue.component('items-component', ItemsComponent);
+Vue.component('change-title-component', ChangeTitleComponent);
+Vue.component('add-item-component', AddItemComponent);
+
+new Vue({
+  el: '#app',
+  data: data,
 })
